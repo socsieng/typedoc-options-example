96c81fe8950b387ba7ee001394d8095ff79d271b
Soc Sieng <socsieng@gmail.com>
2020-01-12 19:48:41 -0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { ConverterComponent } from 'typedoc/dist/lib/converter/components';
+import { Option, Component } from 'typedoc/dist/lib/utils';
+import { Application } from 'typedoc/dist/lib/application';
+
+@Component({ name: 'example' })
+class Plugin extends ConverterComponent {
+  @Option({
+    name: 'message',
+    help: 'outputs a message',
+  } as any)
+  _message!: string;
+}
+
+module.exports = (host: Application) => {
+  console.log('** plugin example **');
+
+  const app = host.owner;
+  app.converter.addComponent('example', new Plugin(app.converter));
+
+  const declaration = app.options.getDeclaration('message');
+
+  console.log('has declaration', !!declaration);
+}
